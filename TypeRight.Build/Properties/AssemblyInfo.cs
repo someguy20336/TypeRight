c60b3c01acd176ab995f4ba3e6ca517e82efc5b1
Dave Whelan <dwhelan2033@gmail.com>
2019-04-14 09:16:52 -0500

    upping some versions for the package, removing some unnecessary stuff

--- TypeRight.Build/Properties/AssemblyInfo.cs
+++ TypeRight.Build/Properties/AssemblyInfo.cs
@@ -19,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5a69b9cb-2b4f-4570-88b6-731ad7ec989d")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -32,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
