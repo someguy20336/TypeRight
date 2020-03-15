74e0befe9a16d059e4098253628c13cc2ee58b6d
Dave Whelan <dwhelan2033@gmail.com>
2020-03-15 10:08:19 -0500

    add schema to json file

--- TypeRight.Core/Configuration/ConfigOptions.cs
+++ TypeRight.Core/Configuration/ConfigOptions.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections.Generic;
-using TypeRight.TypeLocation;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using TypeRight.Configuration.Json;
 
 namespace TypeRight.Configuration
 {
 	/// <summary>
 	/// Represents a configuration file for a solution
 	/// </summary>
+	[JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
 	public class ConfigOptions
 	{
+		[JsonProperty("$schema")]
+		public string Schema { get; set; } 
+			= "https://raw.githubusercontent.com/someguy20336/TypeRight/v0.12/TypeRight.Core/Configuration/typeRightConfig-schema.json";
 
 		/// <summary>
 		/// Gets or sets whether script generation is enabled
@@ -25,6 +31,7 @@ namespace TypeRight.Configuration
 		/// "singleParam" should generally be used for ASP.NET Core applications since there is only one FromBody parameter allowed.  
 		/// "multiParam" will create an object for the parameters.
 		/// </summary>
+		[JsonConverter(typeof(CamelCaseStringEnumConverter))]
 		public ModelBindingType ModelBindingType { get; set; } = ModelBindingType.MultiParam;
 
 		/// <summary>
