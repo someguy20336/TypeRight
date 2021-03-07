b0b4083db9e823bebcf9043be305fb992d37d8d9
Dave Whelan <dwhelan2033@gmail.com>
2021-03-07 14:57:55 -0600

    quick refactoring to simplify some things

--- src/TypeRight.Core/TypeProcessing/ProcessorSettings.cs
+++ src/TypeRight.Core/TypeProcessing/ProcessorSettings.cs
@@ -8,18 +8,6 @@ namespace TypeRight.TypeProcessing
 	/// </summary>
 	public class ProcessorSettings
 	{
-		
-
-		/// <summary>
-		/// Gets or sets the filter to use for the display name attribute
-		/// </summary>
-		public TypeFilter DisplayNameFilter { get; set; }
-
-		/// <summary>
-		/// Gets or sets the filter to use for the MVC action filter
-		/// </summary>
-		public TypeFilter MvcActionFilter { get; set; }
-
 		/// <summary>
 		/// Gets or sets the full path to the project file
 		/// </summary>
@@ -30,13 +18,5 @@ namespace TypeRight.TypeProcessing
 		/// </summary>
 		public string DefaultResultPath { get; set; }
 
-		/// <summary>
-		/// Creates the processor settings object with the default settings
-		/// </summary>
-		public ProcessorSettings()
-		{
-			DisplayNameFilter = new HasInterfaceOfTypeFilter(typeof(IEnumDisplayNameProvider).FullName);
-			MvcActionFilter = new IsOfTypeFilter(typeof(ScriptActionAttribute).FullName);
-		}
 	}
 }
