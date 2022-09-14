b0c117b0f50376dc570d7a038156a7ce71a5a4af
Dave Whelan <dwhelan2033@gmail.com>
2022-09-13 20:49:02 -0500

    add a test controller for new test case for now

---
+++ test/TestProjects/ReplaceWithJsExample/ReplaceWithJsExample/Controllers/RoutelessController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReplaceWithJsExample.Controllers
+{
+    // I tested these routes with Postman - legit
+    [ApiController]
+    [Route("base")]
+    public class RouteTestController : ControllerBase
+    {
+        // GET /api/test/{id}
+        [HttpGet("/api/test/{id}")]
+        public string GetThing(int id)
+        {
+            return $"Got {id}";
+        }
+
+        // GET /base/api/test/{id}
+        [HttpGet("api/test/{id}")]
+        public string GetOtherThing(int id)
+        {
+            return $"Got routeless {id}";
+        }
+    }
+}
