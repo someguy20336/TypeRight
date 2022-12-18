41e63294835a5a4da5195651d769e2b852a036be
Dave Whelan <dwhelan2033@gmail.com>
2022-12-18 09:38:19 -0600

    consolidate nuspecs and use properties to make debug vs prod

--- test/CreateDebugPackage.bat
+++ test/CreateDebugPackage.bat
@@ -2,12 +2,12 @@ ECHO OFF
 
 dotnet build "..\src\TypeRight.Attributes\TypeRight.Attributes.csproj" -c Release
 dotnet build "..\src\TypeRight.Workspaces.VsixAdapter\TypeRight.Workspaces.VsixAdapter.csproj" -c Release
-dotnet publish "..\src\TypeRight\TypeRight.csproj" /p:PublishProfile=Release -f net5.0
+dotnet publish "..\src\TypeRight\TypeRight.csproj" /p:PublishProfile=Release -f net5.0 -c Re
 dotnet publish "..\src\TypeRight\TypeRight.csproj" /p:PublishProfile=Release -f net6.0
 
 rmdir /S /Q %UserProfile%\.nuget\packages\typerightdebug
 rmdir /S /Q .\TestProjects\LocalNugetRepo
 rmdir /Q /S nugetpkgs
-call nuget pack "..\DebugTypeRightNuspec.nuspec" -OutputDirectory nugetpkgs
+call nuget pack "..\TypeRightNuspec.nuspec" -OutputDirectory nugetpkgs -properties id=TypeRightDebug;vers=1.0.3
 
 for %%a in (nugetpkgs\*.nupkg) do call nuget add nugetpkgs\TypeRightDebug.1.0.3.nupkg -Source .\TestProjects\LocalNugetRepo
