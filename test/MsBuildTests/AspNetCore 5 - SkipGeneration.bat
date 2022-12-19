1f1e343a9bcc6801dc2347acc393ad63a8df456b
Dave Whelan <dwhelan2033@gmail.com>
2022-12-19 06:35:37 -0600

    add .net 7, get rid of .net framework project

---
+++ test/MsBuildTests/AspNetCore 5 - SkipGeneration.bat
@@ -0,0 +1,14 @@
+@ECHO off
+
+DEL ..\TestProjects\AspNetCore\TestAspNetCoreApp\Scripts\ServerObjects.* /Q
+DEL ..\TestProjects\AspNetCore\TestAspNetCoreApp\Scripts\CustomGroup.* /Q
+DEL ..\TestProjects\AspNetCore\TestAspNetCoreApp\Scripts\Home\Models.* /Q
+DEL ..\TestProjects\AspNetCore\TestAspNetCoreApp\Scripts\Home\HomeActions.* /Q
+
+..\nuget install TypeRightDebug -DependencyVersion Highest -SolutionDirectory ..\TestProjects\AspNetCore
+
+dotnet build ..\TestProjects\AspNetCore\TestAspNetCoreApp\TestAspNetCoreApp.csproj --force -v n -p:SkipTypeRight=true
+
+ECHO Build Complete!
+
+if NOT "%1" == "1" PAUSE
