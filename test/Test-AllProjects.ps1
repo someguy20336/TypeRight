a553ea04e71826d9e1a541f00a605b49a33b06ae
Dave Whelan <dwhelan2033@gmail.com>
2023-11-15 20:47:14 -0600

    add support for .net 8 (fixes #108)

--- test/Test-AllProjects.ps1
+++ test/Test-AllProjects.ps1
@@ -6,6 +6,8 @@ Write-Host "Testing skip Type Right - there should be no files after this one"
 Read-Host "Press Any Key To continue"
 .\Test-AspNetCore net7.0 "-p:SkipTypeRight=true"
 Read-Host "Press Any Key To continue"
+.\Test-AspNetCore net8.0 "-p:SkipTypeRight=true"
+Read-Host "Press Any Key To continue"
 
 # Standard Tests
 Write-Host "Running standard build in net6.0"
@@ -23,5 +25,12 @@ Write-Host "Printing contents of netstandard lib - there should be things here (
 Get-ChildItem ..\TestProjects\AspNetCore\NetStandardLib\bin\Debug\netstandard2.0
 Read-Host "Press Any Key To continue"
 
+Write-Host "Running standard build in net8.0"
+.\Test-AspNetCore net8.0
+.\Test-ReplaceWithJs net8.0
+
+Write-Host "Printing contents of netstandard lib - there should be things here (https://github.com/daveaglick/Buildalyzer/issues/105)"
+Get-ChildItem ..\TestProjects\AspNetCore\NetStandardLib\bin\Debug\netstandard2.0
+Read-Host "Press Any Key To continue"
 
 Set-Location ..\
