775fdfc4d97d40f1fa252fe37b5b1de2910d3ca7
someguy20336 <38168572+someguy20336@users.noreply.github.com>
2023-12-18 11:00:28 -0600

    Upgrade packages and restructure the NuGet package (#111)
    * upgrade packages
    
    * simplify the nuget package so it doesn't break for each version (fixes #110)

--- test/Test-AllProjects.ps1
+++ test/Test-AllProjects.ps1
@@ -4,8 +4,6 @@ Set-Location .\MsBuildTests
 Write-Host "Testing skip Type Right - there should be no files after this one"
 .\Test-AspNetCore net6.0 "-p:SkipTypeRight=true"
 Read-Host "Press Any Key To continue"
-.\Test-AspNetCore net7.0 "-p:SkipTypeRight=true"
-Read-Host "Press Any Key To continue"
 .\Test-AspNetCore net8.0 "-p:SkipTypeRight=true"
 Read-Host "Press Any Key To continue"
 
@@ -13,14 +11,6 @@ Read-Host "Press Any Key To continue"
 Write-Host "Running standard build in net6.0"
 .\Test-AspNetCore net6.0
 .\Test-ReplaceWithJs net6.0
-Write-Host "Printing contents of netstandard lib - there should be things here (https://github.com/daveaglick/Buildalyzer/issues/105)"
-Get-ChildItem ..\TestProjects\AspNetCore\NetStandardLib\bin\Debug\netstandard2.0
-Read-Host "Press Any Key To continue"
-
-Write-Host "Running standard build in net7.0"
-.\Test-AspNetCore net7.0
-.\Test-ReplaceWithJs net7.0
-
 Write-Host "Printing contents of netstandard lib - there should be things here (https://github.com/daveaglick/Buildalyzer/issues/105)"
 Get-ChildItem ..\TestProjects\AspNetCore\NetStandardLib\bin\Debug\netstandard2.0
 Read-Host "Press Any Key To continue"
