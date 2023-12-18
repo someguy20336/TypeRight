47d2866ec8984fac06b7aee749840c64b543ce11
Dave Whelan <dwhelan2033@gmail.com>
2023-12-18 12:28:23 -0600

    add a test task to see that I get what I think I should get.  And I do!
    Weird issue when debugging where dotnet host doesn't close for some reason

---
+++ src/TypeRight.Build/TestTask.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Build.Framework;
+using System;
+
+namespace TypeRight.Build;
+
+public class TestTask : ITask
+{
+    public IBuildEngine BuildEngine { get; set; }
+    public ITaskHost HostObject { get; set; }
+
+    [Required]
+    public ITaskItem[] ReferencePath { get; set; }
+
+    [Required]
+    public ITaskItem[] Compile { get; set; }
+
+    [Required]
+    public ITaskItem BaseDirectory { get; set; }
+
+    public bool Execute()
+    {
+        return true;
+    }
+}
